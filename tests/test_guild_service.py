7c2878dcb9e40baa6d6bc7bab0d4faab53be79fe
koalth <koalrube@gmail.com>
2024-08-18 20:14:36 -0400

    added pytest

---
+++ tests/test_guild_service.py
@@ -0,0 +1,15 @@
+from src.db import GuildRepository
+from src.services import GuildService
+from src.entities import Guild
+from pytest_mock import mocker
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_guild_service(mocker):
+    mock_response = Guild(name="fake", realm="Dalaran", region="us")
+    repo = GuildRepository()
+    mocker.patch.object(repo, "get_by_guild_name_and_realm", return_value=mock_response)
+    _sut = GuildService(repo)
+
+    assert await _sut.get_by_guild_name_and_realm("fake", "Dalaran") == mock_response
