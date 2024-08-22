d9839622fb0bca1aef008dee2c20f03fd1c9401b
koalth <koalrube@gmail.com>
2024-08-21 21:37:38 -0400

    databasesessionmanager

--- src/db/session.py
+++ src/db/session.py
@@ -27,3 +27,37 @@ class DatabaseSessionManager:
 
         self._engine = None
         self._sessionmaker = None
+
+    @contextlib.asynccontextmanager
+    async def connect(self) -> AsyncIterator[AsyncConnection]:
+        if self._engine is None:
+            raise Exception("DatabaseSessionManager is not intialized")
+
+        async with self._engine.begin() as connection:
+            try:
+                yield connection
+            except Exception:
+                await connection.rollback()
+                raise
+
+    @contextlib.asynccontextmanager
+    async def session(self) -> AsyncIterator[AsyncSession]:
+        if self._sessionmaker is None:
+            raise Exception("DatabaseSessionManager is not intialized")
+
+        session = self._sessionmaker()
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
+
+
+sessionmanager = DatabaseSessionManager(config.DATABASE_URL)
+
+
+async def get_db_session():
+    async with sessionmanager.session() as session:
+        yield session
