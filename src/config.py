4d132ef70910f6a2d004c364741217b1d8b357b7
koalth <koalrube@gmail.com>
2024-08-25 19:56:59 -0400

    config will throw error if vars aren't defined

--- src/config.py
+++ src/config.py
@@ -4,20 +4,17 @@ from pydantic import Field
 
 class Config(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file="dev.env", env_file_encoding="utf-8", extra="ignore"
+        env_file="dev.env",
+        env_file_encoding="utf-8",
+        extra="ignore",
     )
 
     DEBUG: bool = Field(default=False)
 
-    DISCORD_TOKEN: str = Field(default="")
-    DISCORD_CLIENT_ID: str = Field(default="")
+    DISCORD_CLIENT_ID: str = Field(init=False)
+    DISCORD_TOKEN: str = Field(init=False)
 
-    API_URL: str = Field(default="")
-    CALLS: int = Field(default=0)
-    RATE_LIMIT: int = Field(default=0)
-    TIMEOUT: int = Field(default=0)
-    RETRIES: int = Field(default=0)
-    BACKOFF_FACTOR: int = Field(default=0)
+    API_URL: str = Field(init=False)
 
 
 config = Config()
