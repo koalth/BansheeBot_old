f2ed3e15c2176aa0a77787c72c6c6cb384975ff4
koalth <koalrube@gmail.com>
2024-08-26 15:24:45 -0400

    updated config to use default values

--- src/config.py
+++ src/config.py
@@ -4,17 +4,17 @@ from pydantic import Field
 
 class Config(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file="dev.env",
+        env_file=".env",
         env_file_encoding="utf-8",
         extra="ignore",
     )
 
     DEBUG: bool = Field(default=False)
 
-    DISCORD_CLIENT_ID: str = Field(default="", init=False)
-    DISCORD_TOKEN: str = Field(default="", init=False)
+    DISCORD_CLIENT_ID: str = Field(default="client_id", init=False)
+    DISCORD_TOKEN: str = Field(default="token", init=False)
 
-    API_URL: str = Field(default="", init=False)
+    API_URL: str = Field(default="api", init=False)
 
 
 config = Config()
