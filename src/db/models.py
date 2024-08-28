64809254322d0978f689c978c21301e903501fc0
koalth <koalrube@gmail.com>
2024-08-28 10:56:24 -0400

    added settings models, added alembic

--- src/db/models.py
+++ src/db/models.py
@@ -1,10 +1,20 @@
+from uuid import UUID
 from sqlalchemy import ForeignKey, DateTime
 from sqlalchemy.orm import DeclarativeBase, relationship, Mapped, mapped_column
 
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 
 
 class Base(AsyncAttrs, DeclarativeBase):
-    pass
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+
+
+class SettingOrm(Base):
+    __tablename__ = "settings"
+
+    discord_guild_id: Mapped[str] = mapped_column(index=True)
+
+    default_region: Mapped[Optional[str]] = mapped_column(nullable=True)
+    default_realm: Mapped[Optional[str]] = mapped_column(nullable=True)
